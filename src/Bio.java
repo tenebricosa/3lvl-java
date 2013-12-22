60c3f3f9c065a22290f9ec28be84527489efb86a
Сахарова Анастасия <elliot_watson@ymail.com>
2013-12-22 13:46:49 +0600

    Файлы

--- src/Bio.java
+++ src/Bio.java
@@ -1,3 +1,5 @@
+import freemarker.template.TemplateScalarModel;
+
 /**
  * Created with IntelliJ IDEA.
  * User: пользователь
@@ -5,14 +7,35 @@
  * Time: 0:48
  * To change this template use File | Settings | File Templates.
  */
-public class Bio {
+import freemarker.template.*;
+public class Bio implements TemplateScalarModel {
+    public int id;
     public int year;
     public int month;
     public String text;
 
-    public Bio(int year, int month, String text) {
+    public Bio(int id, int year, int month, String text) {
+        this.id = id;
         this.year = year;
         this.month = month;
         this.text = text;
+    }
+
+    @Override
+    public String getAsString() throws TemplateModelException {
+        return id + " " + year + " " + month + " " + text;
+    }
+
+
+    public String getYear() {
+        return "" + year;
+    }
+
+    public String getMonth() {
+        return "" + month;
+    }
+
+    public String getText() {
+        return text;
     }
 }
--- src/Laba_4.java
+++ src/Laba_4.java
@@ -1,16 +1,20 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import freemarker.template.Template;
-import freemarker.template.Configuration;
+import freemarker.template.*;
 
 
 public class Laba_4 extends HTTP {
     Configuration config;
+    String request;
     String url;
+    String method;
+    List<Bio> bio = Arrays.asList(
+            new Bio(1,2010, 12, "note1"),
+            new Bio(2,2009, 4, "note2"));
+//    Map<String, Bio> bio = new HashMap<String, Bio>();
 
     public static void main(String[] args) throws Throwable {
         ServerSocket ss = new ServerSocket(8080); // создаем сокет сервера и привязываем его к вышеуказанному порту
@@ -26,6 +30,7 @@ public class Laba_4 extends HTTP {
         socket = s;
         config = new Configuration();
         config.setClassForTemplateLoading(Laba_4.class, "templates");
+//        config.setObjectWrapper( new DefaultObjectWrapper() );
         config.setDefaultEncoding("UTF-8");
 
 
@@ -33,22 +38,18 @@ public class Laba_4 extends HTTP {
 
     public void run() {
         try {
-            String request = make_request();
-
-            System.out.println(url);
-            url = url.split(" ")[1];
-            String template;
-
+            make_request();
             if (url.contains("static")) {
-//                String path = getClass().getResource(url.replace("\\", "\\\\")).getPath();
                 String path = "C:\\Users\\пользователь\\Documents\\GitHub\\internet\\src";
                 path += url.replace("\\", "\\\\");
                 read_file(path);
+            } else if (url.equals("/api")) {
+                proceed_api();
             } else {
                 if (url.equals("/")) {
                     url = "index.html";
                 }
-                template = render_template(url);
+                String template = render_template(url);
                 make_response(template);
             }
 
@@ -57,16 +58,36 @@ public class Laba_4 extends HTTP {
         }
     }
 
-    public void process_request(String request) {
-         url = request.split("\r\n")[0];
+    public String proceed_api() throws Exception {
+        if (method.equals("GET")) {
+            String result = "";
+            make_response(result);
+
+        } else if (method.equals("POST")) {
+//            Bio o = new Bio(1, 2013, 1, "oLOLOLo");
+//            bio.put("bio", o);
+//            bio.put("qwe", o);
+        } else {
+
+        }
+        return "";
+    }
+
+    public String make_request() throws Exception {
+        request = super.make_request();
+        String first_line = request.split("\r\n")[0];
+        method = first_line.split(" ")[0];
+        url = first_line.split(" ")[1];
+        System.out.println(url);
+        return request;
     }
 
     public String render_template(String path) throws Exception {
-        Map<String, Object> input = new HashMap<String, Object>();
-        input.put("bio", new Bio(2013, 1, "oLOLOLo"));
         Writer out = new StringWriter();
         Template template = config.getTemplate(path);
-        template.process(input, out);
+        Map root = new HashMap();
+        root.put("bio", bio);
+        template.process(root, out);
         return out.toString();
     }
 
@@ -74,7 +95,6 @@ public class Laba_4 extends HTTP {
         OutputStream sout = socket.getOutputStream();
         String response = "HTTP/1.1 200 OK\r\n" +
                 "Server: OloloServer/2013-10-13\r\n" +
-//                "Content-Type: text/html\r\n" +
                 "Content-Length: " + html.getBytes().length + "\r\n" +
                 "Connection: close\r\n\r\n";
         String result = response + html;
@@ -82,39 +102,24 @@ public class Laba_4 extends HTTP {
         sout.flush();
     }
 
-    public void make_error(String message, int code) throws IOException {
-        OutputStream sout = socket.getOutputStream();
-        String response = "HTTP/1.1 +" + code + " OK\r\n" +
-                "Server: OloloServer/2013-10-13\r\n" +
-//                "Content-Type: text/html\r\n" +
-                "Content-Length: " + message.getBytes().length + "\r\n" +
-                "Connection: close\r\n\r\n";
-        String result = response + message;
-        sout.write(result.getBytes());
-        sout.flush();
-    }
-
     public void read_file(String path) throws Throwable {
+        FileReader file = new FileReader(path);
+        BufferedReader br = new BufferedReader(file);
         try {
-            FileReader file = new FileReader(path);
-            BufferedReader br = new BufferedReader(file);
-            try {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-                while (line != null) {
-                    sb.append(line);
-                    sb.append('\n');
-                    line = br.readLine();
-                }
-                String content = sb.toString();
-                make_response(content);
-
-            } finally {
-                br.close();
+            while (line != null) {
+                sb.append(line);
+                sb.append('\n');
+                line = br.readLine();
             }
-        } catch (FileNotFoundException e) {
-            make_error(e.getMessage(), 404);
+            String content = sb.toString();
+            make_response(content);
+
+        } finally {
+            br.close();
         }
+
     }
 }
